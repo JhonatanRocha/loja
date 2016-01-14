4976674464937ddeb04baf4efb824650962dc5ea
JhonatanRocha <jcristianrocha@gmail.com>
2016-01-14 09:32:46 -0200

    Adding before and after annotations on JUnit tests and extracted the startupServer method on Servidor class.

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -1,31 +1,56 @@
 package br.com.alura.loja;
 
+import static org.junit.Assert.assertEquals;
+
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
 
-import junit.framework.Assert;
-
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Projeto;
+
+import com.thoughtworks.xstream.XStream;
 
 public class ClienteTest {
 
-	@Test
-	public void testConnection(){
-		
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://www.mocky.io");
-		String conteudo = target.path("/v2/52aaf5deee7ba8c70329fb7d").request().get(String.class);
-		//System.out.println(conteudo);
-		Assert.assertTrue(conteudo.contains("Rua Vergueiro"));
+	private HttpServer server;
+	
+	@Before
+	public void startupServer(){
+		server = Servidor.startupServer();
+	}
+	
+	@After
+	public void shutdownServer(){
+		server.stop();
 	}
 	
 	@Test
-	public void testProjetoConnection(){
+	public void testCarrinho(){
+		
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		String conteudo = target.path("/carrinhos").request().get(String.class);
+		//System.out.println(conteudo);
+		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
+		
+		assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
+	}
+	
+	@Test
+	public void testProjeto(){
 		
 		Client client = ClientBuilder.newClient();
         WebTarget target = client.target("http://localhost:8080");
         String conteudo = target.path("/projetos").request().get(String.class);
-        Assert.assertTrue(conteudo.contains("<nome>Minha loja"));
+        Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
+
+        assertEquals(1l, projeto.getId());
+        assertEquals("Minha loja", projeto.getNome());
 	}
 }
