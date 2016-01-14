a18ed1547c0562199690fa9029501a0730467161
JhonatanRocha <jcristianrocha@gmail.com>
2016-01-14 14:12:51 -0200

    Adding "toJSON" methods on model classes, adding method to persist on fake db via XML & POST and finally adding testing on each POST methods.

--- src/test/java/br/com/alura/loja/ClienteTest.java
+++ src/test/java/br/com/alura/loja/ClienteTest.java
@@ -4,7 +4,10 @@ import static org.junit.Assert.assertEquals;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -12,6 +15,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 import br.com.alura.loja.modelo.Carrinho;
+import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.modelo.Projeto;
 
 import com.thoughtworks.xstream.XStream;
@@ -20,37 +24,74 @@ public class ClienteTest {
 
 	private HttpServer server;
 	
+	private WebTarget getWebTarget() {
+		Client client = ClientBuilder.newClient();
+		WebTarget target = client.target("http://localhost:8080");
+		return target;
+	}
+	
+	/**
+	 *Executado antes dos testes 
+	 */
 	@Before
 	public void startupServer(){
 		server = Servidor.startupServer();
 	}
 	
+	/**
+	 *Executado após os testes 
+	 */
 	@After
 	public void shutdownServer(){
 		server.stop();
 	}
 	
 	@Test
-	public void testCarrinho(){
+	public void testCarrinhoGET(){
 		
-		Client client = ClientBuilder.newClient();
-		WebTarget target = client.target("http://localhost:8080");
-		String conteudo = target.path("/carrinhos").request().get(String.class);
+		WebTarget target = getWebTarget();
+		String conteudo = target.path("/carrinhos/1").request().get(String.class);
 		//System.out.println(conteudo);
 		Carrinho carrinho = (Carrinho) new XStream().fromXML(conteudo);
 		
 		assertEquals("Rua Vergueiro 3185, 8 andar", carrinho.getRua());
 	}
+
 	
 	@Test
-	public void testProjeto(){
+	public void testProjetoGET(){
 		
-		Client client = ClientBuilder.newClient();
-        WebTarget target = client.target("http://localhost:8080");
-        String conteudo = target.path("/projetos").request().get(String.class);
+		WebTarget target = getWebTarget();
+        String conteudo = target.path("/projetos/1").request().get(String.class);
         Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
 
         assertEquals(1l, projeto.getId());
         assertEquals("Minha loja", projeto.getNome());
+	}
+	
+	@Test
+	public void testCarrinhoPOST(){
+		Carrinho carrinho = new Carrinho();
+        carrinho.adiciona(new Produto(314L, "Tablet", 999, 1));
+        carrinho.setRua("Rua Vergueiro");
+        carrinho.setCidade("Sao Paulo");
+       
+        String xml = carrinho.toXML();
+        
+        Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+        Response response = getWebTarget().path("/carrinhos").request().post(entity);
+        
+        assertEquals("<status>sucesso</status>", response.readEntity(String.class));
+	}
+	
+	@Test
+	public void testProjetoPOST(){
+		Projeto projeto = new Projeto(3l, "Projeto Test", 2016);
+		
+		String xml = projeto.toXML();
+		
+		Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+		Response response = getWebTarget().path("/projetos").request().post(entity);
+		assertEquals("<status>sucesso</status>", response.readEntity(String.class));
 	}
 }
